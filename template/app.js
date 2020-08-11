ae5108cb2455ad92d1e89f4a495663ee5a992c54
Memnoc <smartdroidesign@gmail.com>
2020-08-11 09:30:22 +0100

    new/types finished

--- template/app.js
+++ template/app.js
@@ -1,1 +1,30 @@
+/**
+ * Sample
+ */
+console.log("******** Sample **********");
 console.log("Your TypeScript code goes here");
+console.log("******** ### **********");
+/**
+ * Using types
+ * Key difference: JavaScript uses "dynamic types" (resolved at runtime),
+ * TypeScript uses "static types" (set during development)
+ */
+console.log("******** Using Types **********");
+function add(numberOne, numberTwo, showResult, phrase) {
+    if (showResult) {
+        console.log(phrase, numberOne + numberTwo);
+    }
+    else {
+        return numberOne + numberTwo;
+    }
+}
+/**
+ * Core types
+ * TS does use type inference
+ */
+var numberOne = 5;
+var numberTwo = 3.8;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+add(numberOne, numberTwo, printResult, resultPhrase);
+console.log("******** ### **********");
--- template/app.ts
+++ template/app.ts
@@ -1,1 +1,34 @@
+/**
+ * Sample
+ */
+console.log("******** Sample **********");
 console.log("Your TypeScript code goes here");
+console.log("******** ### **********");
+
+/**
+ * Using types
+ * Key difference: JavaScript uses "dynamic types" (resolved at runtime), 
+ * TypeScript uses "static types" (set during development)
+ */
+console.log("******** Using Types **********");
+function add(numberOne: number, numberTwo: number, showResult: boolean, phrase: string) {
+    if (showResult) {
+        console.log(phrase, numberOne + numberTwo);
+    } else {
+        return numberOne + numberTwo;
+    }
+    
+}
+
+/**
+ * Core types
+ * TS does use type inference 
+ */ 
+const numberOne = 5;
+const numberTwo = 3.8;
+const printResult = true;
+const resultPhrase = 'Result is: '
+
+add(numberOne, numberTwo, printResult, resultPhrase);
+
+console.log("******** ### **********");
